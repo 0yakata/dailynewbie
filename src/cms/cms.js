6cbd0bf743dc2d4cf2f9e0b71633506a16ad994b
satoru <satoru.takasago@gmail.com>
2020-09-09 21:07:53 +0900

    writer add

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,7 +5,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
-import IndexPagePreview from './preview-templates/WriterPagePreview'
+import WriterPagePreview from './preview-templates/WriterPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
