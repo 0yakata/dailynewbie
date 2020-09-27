dc1920ce01888f357ca36e7c56c6f8aa7938d5c7
satoru <satoru.takasago@gmail.com>
2020-09-27 10:07:57 +0900

    ogp

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import PropTypes from 'prop-types'
 
 import Layout from '../components/Layout'
 import BlogRoll from '../components/BlogRoll'
 
 import { graphql } from 'gatsby'
+import { withPrefix } from 'gatsby'
 
 export const IndexPageTemplate = ({
   title
@@ -35,6 +37,19 @@ const IndexPage = ({ data }) => {
     <Layout>
       <IndexPageTemplate
         title={frontmatter.title}
+        helmet={
+          <Helmet titleTemplate="%s | にわかストリートジャーナル">
+            <meta name="description" content={`${frontmatter.description}`} />
+            <meta property="og:site_name" content="にわかストリートジャーナル" />
+            <meta property="og:title" content={frontmatter.title} />
+            <meta property="og:type" content="website" />
+            <meta property="og:image" content={`${withPrefix('/')}img/og-image.jpg`} />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content={frontmatter.title} />
+            <meta name="twitter:description" content={frontmatter.description} />
+            <meta name="twitter:image" content={`${withPrefix('/')}img/og-image.jpg`} />
+          </Helmet>
+        }
       />
     </Layout>
   )
@@ -55,6 +70,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
+        description
       }
     }
   }
